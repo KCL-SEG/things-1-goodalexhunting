71a129b1210253345651bf5896f3581c31571995
Alex San <goodalexhunting@gmail.com>
2022-10-20 19:32:57 +0100

    fixxes

--- things/models.py
+++ things/models.py
@@ -3,12 +3,12 @@ from django.core.validators import MinValueValidator
 from django.core.validators  import MaxValueValidator
 
 # Create your models here.
-class Thing(django.db.models.Model):
+class Thing(models.Model):
     name = models.CharField(max_length = 30, unique=True)
     description = models.TextField(max_length = 120,blank = True, unique = False)
     quantity = models.IntegerField(validators = [
         MinValueValidator(0,message='Cannot be less than 0'),
-        MinValueValidator(100,message='Cannot be more than 100')]
+        MaxValueValidator(100,message='Cannot be more than 100')]
     # ,
     # validators= [RegexValidator(
     #     regex =r'^@\w{3,}$',
