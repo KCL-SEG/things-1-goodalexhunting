955c5b7304ee3e3efa1a5c4393e79619e2216bc9
Alex San <goodalexhunting@gmail.com>
2022-10-20 19:29:25 +0100

    task2,3 attempt 1

--- things/models.py
+++ things/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models import MinValueValidator
+from django.db.models import MaxValueValidator
 
 # Create your models here.
+class Thing(django.db.models.Model):
+    name = models.CharField(max_length = 30, unique=True)
+    description = models.TextField(max_length = 120,blank = True, unique = False)
+    quantity = models.IntegerField(validators = [
+        MinValueValidator(0,message='Cannot be less than 0'),
+        MinValueValidator(100,message='Cannot be more than 100')]
+    # ,
+    # validators= [RegexValidator(
+    #     regex =r'^@\w{3,}$',
+    #     message ='Username must consist of @ symbol followed by atleast 3 alpha numericals'
+    # )]
+    )
