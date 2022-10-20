1011aa615ddfc05660b8c2506f91baca644cff5f
Alex San <goodalexhunting@gmail.com>
2022-10-20 19:41:34 +0100

    task2 attempt '#1

--- things/models.py
+++ things/models.py
@@ -6,13 +6,17 @@ from django.core.validators  import MaxValueValidator
 # Create your models here.
 class Thing(Model):
     name = models.CharField(max_length = 30, unique=True)
-    description = models.TextField(max_length = 120,blank = True, unique = False)
-    quantity = models.IntegerField(validators = [
-        MinValueValidator(0,message='Cannot be less than 0'),
-        MaxValueValidator(100,message='Cannot be more than 100')]
+    description = models.CharField(max_length = 50, unique=True)
+    quantity = models.IntegerField()
+    
+    # name = models.CharField(max_length = 30, unique=True)
+    # description = models.TextField(max_length = 120,blank = True, unique = False)
+    # quantity = models.IntegerField(validators = [
+    #     MinValueValidator(0,message='Cannot be less than 0'),
+    #     MaxValueValidator(100,message='Cannot be more than 100')]
     # ,
     # validators= [RegexValidator(
     #     regex =r'^@\w{3,}$',
     #     message ='Username must consist of @ symbol followed by atleast 3 alpha numericals'
     # )]
-    )
+    # )
